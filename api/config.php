aa6c0014e1620bbeb12535a872f82c717f4b5178
virtualsufi <68276851+virtualsufi@users.noreply.github.com>
2021-04-28 23:06:15 +0300

    Add basic DAO setup plus config file

---
+++ api/config.php
@@ -0,0 +1,10 @@
+<?php
+
+class Config {
+  const DB_HOST = "localhost";
+  const DB_USERNAME = "cen236user";
+  const DB_PASSWORD = "PogurajMe";
+  const DB_SCHEME = "cen236project";
+}
+
+ ?>
---
+++ api/dao/AccountDao.class.php
@@ -0,0 +1,9 @@
+<?php
+
+class AccountDao extends BaseDao{
+
+  public function add_account($account){
+    
+  }
+}
+ ?>
