3c1207d134120b75a7a100919266e2ba5773ebc5
virtualsufi <68276851+virtualsufi@users.noreply.github.com>
2021-04-29 11:14:09 +0300

    Add insert method

--- api/dao/UserDao.class.php
+++ api/dao/UserDao.class.php
@@ -12,5 +12,17 @@ class UserDao extends BaseDao{
     return $this->query_unique("SELECT * FROM users WHERE id = :id", ["id" => $id]);
   }
 
+  public function add_user($user){
+    $sql = "INSERT INTO users (name, email, password, account_id) VALUES (:name, :email, :password, :account_id)";
+    $stmt = $this->connection->prepare($sql);
+    $stmt->execute($user);
+    $user['id'] = $this->connection->lastInsertId();
+    return $user;
+  }
+
+  public function update_user($id, $user){
+
+  }
+
 }
  ?>
--- api/test.php
+++ api/test.php
@@ -8,7 +8,14 @@ require_once dirname(__FILE__). "/dao/UserDao.class.php";
 $user_dao = new UserDao();
 
 //$user = $user_dao->get_user_by_email("alenmustafic@hotmail.com");
-$user = $user_dao->get_user_by_id(15);
+//$user = $user_dao->get_user_by_id(15);
+$user1 = [
+  "name" => "Denis Redzepagic",
+  "email" => "denis@gmail.com",
+  "password" => "MasirajDaDise",
+  "account_id" => 1
+];
+$user = $user_dao->add_user($user1);
 
 print_r($user);
  ?>
