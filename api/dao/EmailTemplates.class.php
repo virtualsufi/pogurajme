eb4ecc0e83a4da82e0f16255b9d02cd9ade3b23a
virtualsufi <68276851+virtualsufi@users.noreply.github.com>
2021-05-02 22:14:31 +0300

    Add more dao files.

--- api/dao/EmailTemplates.class.php
+++ api/dao/EmailTemplates.class.php
@@ -3,6 +3,8 @@ require_once dirname(__FILE__)."/BaseDao.class.php";
 
 class EmailTemplateDao extends BaseDao{
 
-  
+  public function __construct(){
+    parent::__construct("email_templates");
+  }
 }
- ?>
+?>
--- api/test.php
+++ api/test.php
@@ -5,8 +5,22 @@ error_reporting(E_ALL);
 
 require_once dirname(__FILE__). "/dao/UserDao.class.php";
 require_once dirname(__FILE__). "/dao/AccountDao.class.php";
+require_once dirname(__FILE__). "/dao/CampaignDao.class.php";
 
-$dao = new AccountDao();
+$dao = new CampaignDao();
+
+$campaign1 = [
+  "name" => "Bajramsko sniženje",
+  "account_id" => 1,
+  "start_date" => date("Y-m-d H-i-s")
+];
+
+/*$dao->update(1, [
+  "end_date" => "2021-06-01 00:00:00",
+  "status" => "BLOCKED"
+]);*/
+
+//$dao = new AccountDao();
 
 /*$account1 = [
   "name" => "Logosoft",
@@ -14,10 +28,11 @@ $dao = new AccountDao();
 ];
 
 $account = $dao->add($account1);*/
-$dao->update(19, ["name" => "LOGOSOFT"]);
-$accounts = $dao->get_all();
+//$dao->update(19, ["name" => "LOGOSOFT"]);
+//$accounts = $dao->get_all();
+$campaign = $dao->add($campaign1);
 
-print_r($accounts);
+print_r($campaign);
 
 
 //$user = $user_dao->get_user_by_email("alenmustafic@hotmail.com");
